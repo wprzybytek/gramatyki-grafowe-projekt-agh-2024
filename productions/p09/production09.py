20090623716aa2d7ca02b22894e5f0187288198f
Maciej Nessel <maciej.nessel@gmail.com>
2024-12-12 16:43:36 +0100

    add p09

---
+++ productions/p09/production09.py
@@ -0,0 +1,87 @@
+from itertools import combinations
+
+from ..production import Production
+
+
+class ProductionP9(Production):
+    """
+    Production P9:
+    Divides a hexagon into 6 smaller hexagons
+    """
+
+    @property
+    def check(self):
+        """Check if the production can be applied on the selected hexagon."""
+        # Find nodes with R=1 (element marked for splitting) and h=0 for all vertices
+        for node, data in self.graph.nodes(data=True):
+            if data.get("label") == "S" and data.get("R") == 1:
+                neighbors = list(self.graph.neighbors(node))
+                if all(self.graph.nodes[n].get("h") == 0 for n in neighbors):
+                    neighbors_edges_cnt = 0
+                    for n1, n2 in combinations(neighbors, 2):
+                        if self.graph.has_edge(n1, n2):
+                            neighbors_edges_cnt += 1
+                    print(neighbors_edges_cnt)
+                    if neighbors_edges_cnt == 6:
+                        print("prod 09 can be applied to this graph")
+                        return self._extract_subgraph(node, neighbors)
+        print("prod 09 can't be applied to this graph")
+        return None
+
+    def apply(self):
+        """Apply P9 to divide the hexagon"""
+        result = self.check
+        if result:
+            q_node, neighbors = result
+
+            self.subgraph.remove_node(q_node)
+            self.graph.remove_node(q_node)
+
+            midpoints = {}
+            for n1, n2 in combinations(neighbors, 2):
+                if self.subgraph.get_edge_data(n1, n2):
+                    self._create_midpoint(midpoints, n1, n2)
+
+            self._fill_graph(neighbors, midpoints)
+            self.graph.update(self.subgraph)
+
+
+# paste this to main class to see how this production works
+"""
+P:5.0:5.0
+    G = nx.Graph()
+    G.add_node("S:5.0:5.0", label="S", R=1)
+    G.add_nodes_from(
+        [
+            ("v:0.0:0.0", {"label": "v", "x": 0.0, "y": 0.0, "h": 0}),
+            ("v:10.0:0.0", {"label": "v", "x": 10.0, "y": 0.0, "h": 0}),
+            ("v:10.0:10.0", {"label": "v", "x": 10.0, "y": 10.0, "h": 0}),
+            ("v:0.0:10.0", {"label": "v", "x": 0.0, "y": 10.0, "h": 0}),
+            ("v:15.0:5.0", {"label": "v", "x": 15.0, "y": 5.0, "h": 0}),
+            ("v:15.0:15.0", {"label": "v", "x": 15.0, "y": 15.0, "h": 0}),
+        ]
+    )
+    G.add_edges_from(
+        [
+            ("v:0.0:0.0", "v:10.0:0.0", {"label": "E", "B": 1}),
+            ("v:15.0:5.0", "v:10.0:10.0", {"label": "E", "B": 1}),
+            ("v:10.0:0.0", "v:15.0:5.0", {"label": "E", "B": 1}),
+            ("v:10.0:10.0", "v:0.0:10.0", {"label": "E", "B": 1}),
+            ("v:0.0:10.0", "v:15.0:15.0", {"label": "E", "B": 1}),
+            ("v:15.0:15.0", "v:0.0:0.0", {"label": "E", "B": 1}),
+
+            ("S:5.0:5.0", "v:0.0:0.0"),
+            ("S:5.0:5.0", "v:15.0:5.0"),
+            ("S:5.0:5.0", "v:10.0:0.0"),
+            ("S:5.0:5.0", "v:10.0:10.0"),
+            ("S:5.0:5.0", "v:0.0:10.0"),
+            ("S:5.0:5.0", "v:15.0:15.0"),
+
+        ]
+    )
+
+    plot_graph(G)
+    prod9 = ProductionP9(G)
+    prod9.apply()
+    plot_graph(G)
+"""
---
+++ productions/p09/tests09.py
@@ -0,0 +1,58 @@
+import pytest
+import networkx as nx
+
+
+from productions.p09.production09 import ProductionP9
+
+def prepare_valid_test_graph() -> nx.Graph:
+    G = nx.Graph()
+    G.add_node("S:5.0:5.0", label="S", R=1)
+    G.add_nodes_from(
+        [
+            ("v:0.0:0.0", {"label": "v", "x": 0.0, "y": 0.0, "h": 0}),
+            ("v:10.0:0.0", {"label": "v", "x": 10.0, "y": 0.0, "h": 0}),
+            ("v:10.0:10.0", {"label": "v", "x": 10.0, "y": 10.0, "h": 0}),
+            ("v:0.0:10.0", {"label": "v", "x": 0.0, "y": 10.0, "h": 0}),
+            ("v:15.0:5.0", {"label": "v", "x": 15.0, "y": 5.0, "h": 0}),
+            ("v:15.0:15.0", {"label": "v", "x": 15.0, "y": 15.0, "h": 0}),
+        ]
+    )
+    G.add_edges_from(
+        [
+            ("v:0.0:0.0", "v:10.0:0.0", {"label": "E", "B": 1}),
+            ("v:15.0:5.0", "v:10.0:10.0", {"label": "E", "B": 1}),
+            ("v:10.0:0.0", "v:15.0:5.0", {"label": "E", "B": 1}),
+            ("v:10.0:10.0", "v:0.0:10.0", {"label": "E", "B": 1}),
+            ("v:0.0:10.0", "v:15.0:15.0", {"label": "E", "B": 1}),
+            ("v:15.0:15.0", "v:0.0:0.0", {"label": "E", "B": 1}),
+
+            ("S:5.0:5.0", "v:0.0:0.0"),
+            ("S:5.0:5.0", "v:15.0:5.0"),
+            ("S:5.0:5.0", "v:10.0:0.0"),
+            ("S:5.0:5.0", "v:10.0:10.0"),
+            ("S:5.0:5.0", "v:0.0:10.0"),
+            ("S:5.0:5.0", "v:15.0:15.0"),
+
+        ]
+    )
+
+    return G
+
+def prepare_valid_test_graph_with_hanging_node() -> nx.Graph:
+    pass
+
+
+@pytest.fixture(scope="function")
+def prepare_graph_positive():
+    yield prepare_valid_test_graph()
+
+
+@pytest.fixture(scope="function")
+def prepare_graph_positive_hanging():
+    yield prepare_valid_test_graph_with_hanging_node()
+
+
+def test_positive_p09_check(prepare_graph_positive: nx.Graph):
+    """check is not None if the graph is valid"""
+    assert ProductionP9(prepare_graph_positive).check is not None
+
