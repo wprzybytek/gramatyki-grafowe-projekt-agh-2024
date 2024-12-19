8fe68988dbf259acd3f24d0ad9bb37891e4bed22
Maciej Nessel <maciej.nessel@gmail.com>
2024-12-19 20:13:42 +0100

    add test graphs

--- productions/p09/tests09.py
+++ productions/p09/tests09.py
@@ -4,55 +4,80 @@ import networkx as nx
 
 from productions.p09.production09 import ProductionP9
 
-def prepare_valid_test_graph() -> nx.Graph:
+def get_graph_without_hanging_node(x_shift=0, y_shift=0):
     G = nx.Graph()
-    G.add_node("P:5.0:5.0", label="P", R=1)
+    G.add_node(f"P:{5.0 + x_shift}:{5.0 + y_shift}", label="P", R=1)
     G.add_nodes_from(
         [
-            ("v:0.0:0.0", {"label": "v", "x": 0.0, "y": 0.0, "h": 0}),
-            ("v:10.0:0.0", {"label": "v", "x": 10.0, "y": 0.0, "h": 0}),
-            ("v:10.0:10.0", {"label": "v", "x": 10.0, "y": 10.0, "h": 0}),
-            ("v:0.0:10.0", {"label": "v", "x": 0.0, "y": 10.0, "h": 0}),
-            ("v:15.0:5.0", {"label": "v", "x": 15.0, "y": 5.0, "h": 0}),
-            ("v:15.0:15.0", {"label": "v", "x": 15.0, "y": 15.0, "h": 0}),
+            (f"v:{10 + x_shift}:{5 + y_shift}", {"label": "v", "x": 10 + x_shift, "y": 5 + y_shift, "h": 0}),
+            (f"v:{8 + x_shift}:{9 + y_shift}", {"label": "v", "x": 8 + x_shift, "y": 9 + y_shift, "h": 0}),
+            (f"v:{3 + x_shift}:{9 + y_shift}", {"label": "v", "x": 3 + x_shift, "y": 9 + y_shift, "h": 0}),
+            (f"v:{0 + x_shift}:{5 + y_shift}", {"label": "v", "x": 0 + x_shift, "y": 5 + y_shift, "h": 0}),
+            (f"v:{2 + x_shift}:{1 + y_shift}", {"label": "v", "x": 2 + x_shift, "y": 1 + y_shift, "h": 0}),
+            (f"v:{8 + x_shift}:{1 + y_shift}", {"label": "v", "x": 8 + x_shift, "y": 1 + y_shift, "h": 0}),
         ]
     )
     G.add_edges_from(
         [
-            ("v:0.0:0.0", "v:10.0:0.0", {"label": "E", "B": 1}),
-            ("v:15.0:5.0", "v:10.0:10.0", {"label": "E", "B": 1}),
-            ("v:10.0:0.0", "v:15.0:5.0", {"label": "E", "B": 1}),
-            ("v:10.0:10.0", "v:0.0:10.0", {"label": "E", "B": 1}),
-            ("v:0.0:10.0", "v:15.0:15.0", {"label": "E", "B": 1}),
-            ("v:15.0:15.0", "v:0.0:0.0", {"label": "E", "B": 1}),
+            (f"v:{10 + x_shift}:{5 + y_shift}", f"v:{8 + x_shift}:{9 + y_shift}", {"label": "E", "B": 1}),
+            (f"v:{8 + x_shift}:{9 + y_shift}", f"v:{3 + x_shift}:{9 + y_shift}", {"label": "E", "B": 1}),
+            (f"v:{3 + x_shift}:{9 + y_shift}", f"v:{0 + x_shift}:{5 + y_shift}", {"label": "E", "B": 1}),
+            (f"v:{0 + x_shift}:{5 + y_shift}", f"v:{2 + x_shift}:{1 + y_shift}", {"label": "E", "B": 1}),
+            (f"v:{2 + x_shift}:{1 + y_shift}", f"v:{8 + x_shift}:{1 + y_shift}", {"label": "E", "B": 1}),
+            (f"v:{8 + x_shift}:{1 + y_shift}", f"v:{10 + x_shift}:{5 + y_shift}", {"label": "E", "B": 1}),
 
-            ("P:5.0:5.0", "v:0.0:0.0"),
-            ("P:5.0:5.0", "v:15.0:5.0"),
-            ("P:5.0:5.0", "v:10.0:0.0"),
-            ("P:5.0:5.0", "v:10.0:10.0"),
-            ("P:5.0:5.0", "v:0.0:10.0"),
-            ("P:5.0:5.0", "v:15.0:15.0"),
-
+            (f"P:{5.0 + x_shift}:{5.0 + y_shift}", f"v:{10 + x_shift}:{5 + y_shift}"),
+            (f"P:{5.0 + x_shift}:{5.0 + y_shift}", f"v:{8 + x_shift}:{9 + y_shift}"),
+            (f"P:{5.0 + x_shift}:{5.0 + y_shift}", f"v:{3 + x_shift}:{9 + y_shift}"),
+            (f"P:{5.0 + x_shift}:{5.0 + y_shift}", f"v:{0 + x_shift}:{5 + y_shift}"),
+            (f"P:{5.0 + x_shift}:{5.0 + y_shift}", f"v:{2 + x_shift}:{1 + y_shift}"),
+            (f"P:{5.0 + x_shift}:{5.0 + y_shift}", f"v:{8 + x_shift}:{1 + y_shift}"),
         ]
     )
+    return G
+
+def get_graph_with_1_hanging_node(x_shift=0, y_shift=0):
+    G = get_graph_without_hanging_node(x_shift, y_shift)
+    G.nodes[f"v:{8 + x_shift}:{9 + y_shift}"]["h"] = 1
+    return G
+
+def get_graph_with_2_hanging_nodes(x_shift=0, y_shift=0):
+    G = get_graph_without_hanging_node(x_shift, y_shift)
+    G.nodes[f"v:{8 + x_shift}:{9 + y_shift}"]["h"] = 1
+    G.nodes[f"v:{3 + x_shift}:{9 + y_shift}"]["h"] = 1
+    return G
+
+def get_big_graph_with_2_hanging_nodes(x_shift=0, y_shift=0):
+    G1 = get_graph_with_2_hanging_nodes(x_shift, y_shift)
+    G2 = get_graph_with_2_hanging_nodes(x_shift + 20, y_shift)
+
+    G = nx.compose(G1, G2)
+    G.add_edge(f"v:{10 + x_shift}:{5 + y_shift}", f"v:{0 + x_shift + 20}:{5 + y_shift}", label="E", B=1)
 
     return G
 
-def prepare_valid_test_graph_with_hanging_node() -> nx.Graph:
-    pass
+def get_two_hexagons_without_hanging_node_with_common_edge(x_shift=0, y_shift=0):
+    G1 = get_graph_without_hanging_node(x_shift, y_shift)
+    G2 = get_graph_without_hanging_node(x_shift + 10, y_shift)
 
+    G = nx.compose(G1, G2)
 
-@pytest.fixture(scope="function")
-def prepare_graph_positive():
-    yield prepare_valid_test_graph()
+    return G
 
+def get_two_hexagons_with_1_hanging_node_with_common_edge(x_shift=0, y_shift=0):
+    G1 = get_graph_with_1_hanging_node(x_shift, y_shift)
+    G2 = get_graph_with_1_hanging_node(x_shift + 10, y_shift)
 
-@pytest.fixture(scope="function")
-def prepare_graph_positive_hanging():
-    yield prepare_valid_test_graph_with_hanging_node()
+    G = nx.compose(G1, G2)
 
+    return G
 
-def test_positive_p09_check(prepare_graph_positive: nx.Graph):
+def test_positive_p09_check():
     """check is not None if the graph is valid"""
-    assert ProductionP9(prepare_graph_positive).check is not None
+    G = get_graph_without_hanging_node()
+    assert ProductionP9(G).check is not None
 
+def test_negative_p09_check():
+    """check is not None if the graph is valid"""
+    G = get_graph_with_1_hanging_node()
+    assert ProductionP9(G).check is None
